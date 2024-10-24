599646a876bf8ed7203bdc8e74ff5397ae650f31
dmisl <rithlesscorp@gmail.com>
2024-10-24 13:27:14 +0200

    payments table filling added to database.php script

--- database.php
+++ database.php
@@ -12,58 +12,60 @@ if(is_file($faker_path))
   include $faker_path;
 }
 
-echo str_repeat("=", 100)."\n";
-echo "Przede wszystkim w przypadku wystąpienia błędu proszę państwo zmienić ustawinia połączenia z serwerem w pliku database.php\n";
-echo str_repeat("=", 100)."\n";
+// echo str_repeat("=", 100)."\n";
+// echo "Przede wszystkim w przypadku wystąpienia błędu proszę państwo zmienić ustawinia połączenia z serwerem w pliku database.php\n";
+// echo str_repeat("=", 100)."\n";
 
-// POŁĄCZAMY SIĘ Z SERWEREM (JEŻELI PAŃTWO MA INNE USTAWIENIA - PROSZE ZMIENIĆ)
+// // POŁĄCZAMY SIĘ Z SERWEREM (JEŻELI PAŃTWO MA INNE USTAWIENIA - PROSZE ZMIENIĆ)
 
   $connection = mysqli_connect('localhost', 'root', '');
 
-// TWORZYMY BAZE DANYCH
-  // function create_database($name, $connection)
-  // {
-  //   try {
-  //     $query = "CREATE DATABASE $name";
-  //     $connection->query($query);
-  //     echo "Baza danych pod nazwą $name była utworzona \n";
-  //     $connection->select_db($name);
-  //     return $connection;
-  //   } catch (mysqli_sql_exception $e) {
-  //     echo "Wygląda na to że baza danych już pod nazwą \"$name\" istnieje \n";
-  //     echo "Wpisz inną nazwe nowej bazy danych \n";
-  //     echo "Nazwa nowej bazy danych: ";
-  //     return create_database(trim(fgets(STDIN)), $connection);
-  //   }
-  // }
+// // TWORZYMY BAZE DANYCH
+//   // function create_database($name, $connection)
+//   // {
+//   //   try {
+//   //     $query = "CREATE DATABASE $name";
+//   //     $connection->query($query);
+//   //     echo "Baza danych pod nazwą $name była utworzona \n";
+//   //     $connection->select_db($name);
+//   //     return $connection;
+//   //   } catch (mysqli_sql_exception $e) {
+//   //     echo "Wygląda na to że baza danych już pod nazwą \"$name\" istnieje \n";
+//   //     echo "Wpisz inną nazwe nowej bazy danych \n";
+//   //     echo "Nazwa nowej bazy danych: ";
+//   //     return create_database(trim(fgets(STDIN)), $connection);
+//   //   }
+//   // }
 
-  // echo "\n\n".str_repeat("=", 100)."\n\n";
-  // echo "1/2 Tworzymy bazy danych\n";
+//   // echo "\n\n".str_repeat("=", 100)."\n\n";
+//   // echo "1/2 Tworzymy bazy danych\n";
 
-  // $connection = create_database('zadanie', $connection);
+//   // $connection = create_database('zadanie', $connection);
 
-  // echo "2/2 Baza danych była utworzona\n";
-  // echo "\n\n".str_repeat("=", 100)."\n\n";
+//   // echo "2/2 Baza danych była utworzona\n";
+//   // echo "\n\n".str_repeat("=", 100)."\n\n";
 
-// ZAPEŁNIAMY STRUKTURE BAZY (JA BYM JĄ OCZEWIŚCIE NORMALIZOWAŁ, ALE W ŻYCIU JEST TAK, ŻE GRUBY ROBI TO CO MU POWIEDZĄ)
+// // ZAPEŁNIAMY STRUKTURE BAZY (JA BYM JĄ OCZEWIŚCIE NORMALIZOWAŁ, ALE W ŻYCIU JEST TAK, ŻE GRUBY ROBI TO CO MU POWIEDZĄ)
 
-  // echo "\n\n".str_repeat("=", 100)."\n\n";
-  // echo "Tworzymy tabeli i ich struktury...\n";
+//   $connection->select_db('zadanie');
 
-  // $klienci_structure = "CREATE TABLE `klienci` (
-  //   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  //   `login` varchar(255) NOT NULL,
-  //   `konto_bankowe` varchar(28) NOT NULL,
-  //   `NIP` varchar(10) NOT NULL,
-  //   PRIMARY KEY (`id`)
-  // )";
+//   echo "\n\n".str_repeat("=", 100)."\n\n";
+//   echo "Tworzymy tabeli i ich struktury...\n";
 
-  // $connection->query($klienci_structure);
+  $klienci_structure = "CREATE TABLE `klienci` (
+    `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+    `login` varchar(255) NOT NULL,
+    `konto_bankowe` varchar(28) NOT NULL,
+    `NIP` varchar(10) NOT NULL,
+    PRIMARY KEY (`id`)
+  )";
 
-  // echo "1/4 Klienci już są\n";
+//   $connection->query($klienci_structure);
+
+//   // echo "1/4 Klienci już są\n";
 
   // $faktury_structure = "CREATE TABLE `faktury` (
-  //   `numer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  //   `numer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   //   `data_wystawienia` date NOT NULL,
   //   `termin_platnosci` date NOT NULL,
   //   `suma_brutto` decimal(10,2) NOT NULL,
@@ -72,9 +74,9 @@ echo str_repeat("=", 100)."\n";
   //   KEY `klient_id` (`klient_id`)
   // )";
 
-  // $connection->query($faktury_structure);
+//   $connection->query($faktury_structure);
 
-  // echo "2/4 Faktury połaczyły się\n";
+//   // echo "2/4 Faktury połaczyły się\n";
 
   // $pozycje_faktury_structure = "CREATE TABLE `pozycje_faktury` (
   //   `id` int NOT NULL AUTO_INCREMENT,
@@ -86,9 +88,9 @@ echo str_repeat("=", 100)."\n";
   //   KEY `numer_faktury` (`numer_faktury`)
   // )";
 
-  // $connection->query($pozycje_faktury_structure);
+//   $connection->query($pozycje_faktury_structure);
 
-  // echo "3/4 Pozycjy faktury są\n";
+//   // echo "3/4 Pozycjy faktury są\n";
 
   // $platnosci_structure = "CREATE TABLE `platnosci` (
   //   `id` int NOT NULL AUTO_INCREMENT,
@@ -101,74 +103,87 @@ echo str_repeat("=", 100)."\n";
   //   KEY `numer_faktury` (`numer_faktury`)
   // )";
 
-  // $connection->query($platnosci_structure);
+//   $connection->query($platnosci_structure);
 
-  // echo "4/4 Płatności mowią że to się wjeżdża\n";
+//   echo "4/4 Płatności mowią że to się wjeżdża\n";
 
-  // echo "Struktura bazy danych jest gotowa do korzystania";
+//   echo "Struktura bazy danych jest gotowa do korzystania";
 
-  // echo "\n\n".str_repeat("=", 100)."\n\n";
+//   echo "\n\n".str_repeat("=", 100)."\n\n";
 
-// ZAPEWNIAMY TABELE DANYMI
+// // ZAPEWNIAMY TABELE DANYMI
 
-  $connection->select_db("zadanie");
+//   echo "Zapewniamy tabele przykładowymi danymi\n";
+//   echo "1/4 Zapewniamy klientów\n";
 
-  echo "\n\n".str_repeat("=", 100)."\n\n";
-  echo "Zapewniamy tabele przykładowymi danymi\n";
-  echo "1/4 Zapewniamy klientów\n";
+//   // KORZYSTAJAC Z FAKERA TWORZYMY 20 KLIENTÓW
+//     $faker = Faker\Factory::create();
 
-  // KORZYSTAJAC Z FAKERA TWORZYMY 20 KLIENTÓW
-    $faker = Faker\Factory::create();
+//     $klienci = [];
+//     for ($i = 0; $i < 20; $i++) {
+//       $accountNumber = 'PL';
+//       for ($i2 = 0; $i2 < 26; $i2++) {
+//           $accountNumber .= rand(0, 9);
+//       }
+//       $klienci[] = "('" . $faker->userName() . "', '" . $accountNumber . "', '" . str_pad(rand(0, 9999999999), 10, '0', STR_PAD_LEFT) . "')";
+//     }
 
-    // $klienci = [];
-    // for ($i = 0; $i < 20; $i++) {
-    //   $accountNumber = 'PL';
-    //   for ($i2 = 0; $i2 < 26; $i2++) {
-    //       $accountNumber .= rand(0, 9);
-    //   }
-    //   $klienci[] = "('" . $faker->userName() . "', '" . $accountNumber . "', '" . str_pad(rand(0, 9999999999), 10, '0', STR_PAD_LEFT) . "')";
-    // }
+//     $connection->query("INSERT INTO klienci (login, konto_bankowe, NIP) VALUES " . implode(", ", $klienci));
 
-    // $connection->query("INSERT INTO klienci (login, konto_bankowe, NIP) VALUES " . implode(", ", $klienci));
+//   echo "2/4 Zapewniamy klientowskie faktury\n";
 
-  echo "2/4 Zapewniamy klientowskie faktury\n";
+//   // TWORZYMY PO 3 FAKTURY DLA KAŻDEGO KLIENTA
+//     $result = $connection->query('SELECT id FROM klienci');
+//     $faktury = [];
+//     while($row = mysqli_fetch_assoc($result))
+//     {
+//       for ($i = 0; $i < 3; $i++) {
+//         $rok = date('Y');
+//         $miesiac = date('m');
+//         $numerPorzadkowy = str_pad(count($faktury)+1, 4, '0', STR_PAD_LEFT);
+//         $numer = "FV/$rok/$miesiac/$numerPorzadkowy";
+//         $data_wystawienia = $faker->date();
+//         $termin_platnosci = date('Y-m-d', strtotime($data_wystawienia . ' + 14 days'));
+//         $suma_brutto = $faker->randomFloat(2, 100, 1000);
 
-  // TWORZYMY PO 3 FAKTURY DLA KAŻDEGO KLIENTA
-    // $result = $connection->query('SELECT id FROM klienci');
-    // $faktury = [];
-    // while($row = mysqli_fetch_assoc($result))
-    // {
-    //   for ($i = 0; $i < 3; $i++) {
-    //     $rok = date('Y');
-    //     $miesiac = date('m');
-    //     $numerPorzadkowy = str_pad(count($faktury)+1, 4, '0', STR_PAD_LEFT);
+//         $faktury[] = "('$numer', '$data_wystawienia', '$termin_platnosci', '$suma_brutto', {$row['id']})";
+//       }
+//     }
+//     $connection->query("INSERT INTO faktury (numer, data_wystawienia, termin_platnosci, suma_brutto, klient_id) VALUES " . implode(", ", $faktury));
 
-    //     $numer = "FV/$rok/$miesiac/$numerPorzadkowy";
-    //     $data_wystawienia = $faker->date();
-    //     $termin_platnosci = date('Y-m-d', strtotime($data_wystawienia . ' + 14 days'));
-    //     $suma_brutto = $faker->randomFloat(2, 100, 1000);
-
-    //     $faktury[] = "('$numer', '$data_wystawienia', '$termin_platnosci', '$suma_brutto', {$row['id']})";
-    //   }
-    // }
-    // $connection->query("INSERT INTO faktury (numer, data_wystawienia, termin_platnosci, suma_brutto, klient_id) VALUES " . implode(", ", $faktury));
-
-  echo "3/4 Zapewniamy klientowskie faktury\n";
-  // DLA KAŻDEJ FAKTURY TWORZYMY JĄ POZYCJE
-    // $result = $connection->query('SELECT numer FROM faktury');
-    // $pozycje_faktur = [];
-    // while($row = mysqli_fetch_assoc($result))
-    // {
-    //   $ilosc_pozycji = rand(1, 3);
-    //   for ($i = 0; $i < $ilosc_pozycji; $i++) {
-    //       $nazwa_produktu = $faker->word();
-    //       $ilosc = rand(1, 10);
-    //       $cena = $faker->randomFloat(2, 10, 500);
+//   echo "3/4 Zapewniamy klientowskie faktury\n";
+//   // DLA KAŻDEJ FAKTURY TWORZYMY JĄ POZYCJE
+//     $result = $connection->query('SELECT numer FROM faktury');
+//     $pozycje_faktur = [];
+//     while($row = mysqli_fetch_assoc($result))
+//     {
+//       $ilosc_pozycji = rand(1, 3);
+//       for ($i = 0; $i < $ilosc_pozycji; $i++) {
+//           $nazwa_produktu = $faker->word();
+//           $ilosc = rand(1, 10);
+//           $cena = $faker->randomFloat(2, 10, 500);
   
-    //       $pozycje_faktur[] = "('$nazwa_produktu', $ilosc, $cena, '{$row['numer']}')";
-    //   }
-    // }
-    // $connection->query("INSERT INTO pozycje_faktury (nazwa_produktu, ilosc, cena, numer_faktury) VALUES " . implode(", ", $pozycje_faktur));
+//           $pozycje_faktur[] = "('$nazwa_produktu', $ilosc, $cena, '{$row['numer']}')";
+//       }
+//     }
+//     $connection->query("INSERT INTO pozycje_faktury (nazwa_produktu, ilosc, cena, numer_faktury) VALUES " . implode(", ", $pozycje_faktur));
+    echo "4/4 Zapewniamy płatności\n";
+
+    $connection->select_db("zadanie");
+
+    // DLA KAŻDEJ FAKTURY ROBIMY JĄ PŁATNOŚĆ PRZEZ KLIENTA
+    $result = $connection->query("SELECT faktury.numer AS numer_faktury, faktury.suma_brutto AS kwota, klienci.konto_bankowe AS numer_konta, klienci.login AS klient_login, faktury.data_wystawienia AS data_wplaty FROM faktury JOIN klienci ON faktury.klient_id = klienci.id;");
+    $platnosci = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+      $tytul = "Jakiś tytuł wpisany przez klienta";
+      $kwota = round($row['kwota']*rand(5,10)/10, 2); // dodajemy losową kwotę którą klient zapłacił
+      $data_wplaty = $row['data_wplaty'];
+      $numer_konta = $row['numer_konta'];
+      $numer_faktury = $row['numer_faktury'];
+
+      $platnosci[] = "('$tytul', $kwota, $data_wplaty, '$numer_konta', '$numer_faktury')";
+    }
+    $connection->query("INSERT INTO platnosci (tytul, kwota, data_wplaty, numer_konta, numer_faktury) VALUES " . implode(", ", $platnosci));
 
 // ZAMYKAMY POŁĄCZENIE
   $connection->close();
---
+++ index.php
@@ -0,0 +1,5 @@
+<div style="width: 100%; height: 100vh; margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; flex-direction: column; font-size: 40px; font-weight: 500;">
+    
+    <?=round(992.83*rand(0,10)/10, 2)?>
+
+</div>
