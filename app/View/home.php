6131b20edd183e6cb9f763d07aa2a0671fbf09b2
dmisl <rithlesscorp@gmail.com>
2024-10-25 00:00:56 +0200

    css handling

--- app/View/home.php
+++ app/View/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="storage/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
@@ -13,7 +13,7 @@
 
         <div class="account__badge__parent">
             <div class="account__badge">
-                <img class="avatar" src="img/avatar.jpg">
+                <img class="avatar" src="storage/img/avatar.jpg">
                 <select class="form-select">
                     <option value="">Ivan Something</option>
                 </select>
