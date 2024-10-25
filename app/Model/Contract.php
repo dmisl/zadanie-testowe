c210edef243b1866046dda977c93d7aea0e100ca
dmisl <rithlesscorp@gmail.com>
2024-10-25 14:12:15 +0200

    second task contact model added

---
+++ app/Model/Contract.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model;
+
+class Contract
+{
+    private $connection;
+
+    public function __construct(array $config)
+    {
+        $this->connection = new Database();
+    }
+
+    public function getAmount($sort)
+    {
+        $query = "SELECT * FROM contracts WHERE id = ? AND kwota > 10";
+        
+        if ($sort == 'DESC') {
+            $query .= " ORDER BY nazwa_przedsiebiorcy DESC, NIP DESC";
+        } elseif ($sort == 'ASC') {
+            $query .= " ORDER BY kwota";
+        }
+
+        $result = $this->connection->query($query);
+
+        return $result;
+    }
+
+    public function all()
+    {
+        $query = "SELECT * FROM contracts ORDER BY id";
+        
+        $result = $this->connection->query($query);
+
+        return $result;
+    }
+
+}
